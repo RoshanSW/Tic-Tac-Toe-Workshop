575c9c0ad2ee24826edb14b6d1c7c35cd16c2032
roshansw <roshanwalzade2@gmail.com>
2022-05-25 14:26:26 +0530

    Set the char[10] starting from 0

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -2,4 +2,21 @@ package TicTacToe;
 
 public class TicTacToe {
 
+    char[] javaCharArray = new char[10];
+
+    void arrayInitialize()
+    {
+
+        for(int i =1 ; i < javaCharArray.length; i++)
+        {
+            javaCharArray[i] = ' ';
+        }
+
+    }
+
+    public static void main(String args[])
+    {
+        TicTacToe t = new TicTacToe();
+        t.arrayInitialize();
+    }
 }
